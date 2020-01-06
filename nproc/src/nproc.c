17f47f4b013022833f96bd19cf1d846e43206b0f
Tomas El Fakih <tomaselfakih@gmail.com>
2020-01-06 18:20:46 -0400

    Mejor documentacion

--- nproc/src/nproc.c
+++ nproc/src/nproc.c
@@ -3,7 +3,7 @@
 #include <sys/resource.h>
 
 int main(int argc, char **argv) {
-	struct rlimit rlim;
-	getrlimit(RLIMIT_NPROC, &rlim);
-	printf("%lu\n", rlim.rlim_max);
+	struct rlimit rlim; // Estructura que almacena los limites de procesamiento
+	getrlimit(RLIMIT_NPROC, &rlim); // Hacemos una llamada al sistema con la api de linux
+	printf("Numero de procesos que puede tener el usuario en activo: %lu\n", rlim.rlim_max);
 }
